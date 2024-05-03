4c569b52509eeb43c8d3973d1f53e9f6d1a9e2ba
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-05-03 14:33:32 -0400

    Update main.tf

--- EC2 servicio/terraform-code/main.tf
+++ EC2 servicio/terraform-code/main.tf
@@ -1,1 +1,12 @@
+resource "aws_instance" "my_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  security_groups = var.security_groups
+  subnet_id     = var.subnet_id
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
 
