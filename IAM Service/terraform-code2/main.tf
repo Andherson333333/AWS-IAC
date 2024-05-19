ee1aff569cccf89fd97f4f13a06d0ca65a55319a
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-05-19 11:43:49 -0400

    Update main.tf

--- IAM Service/terraform-code2/main.tf
+++ IAM Service/terraform-code2/main.tf
@@ -1,13 +1,16 @@
+## Crear usuario
 resource "aws_iam_user" "my_user" {
   name = var.user_name
 }
 
+## Configuracion profile contraseña
 resource "aws_iam_user_login_profile" "my_user_login_profile" {
   user                    = aws_iam_user.my_user.name
   password_length         = 12  
   password_reset_required = true
 }
 
+## Politica del usuario
 resource "aws_iam_policy" "my_policy" {
   name   = "my-policy"
   policy = <<EOF
@@ -24,6 +27,7 @@ resource "aws_iam_policy" "my_policy" {
 EOF
 }
 
+## Atachar la policita al usuario
 resource "aws_iam_user_policy_attachment" "my_policy_attachment" {
   user       = aws_iam_user.my_user.name
   policy_arn = aws_iam_policy.my_policy.arn
