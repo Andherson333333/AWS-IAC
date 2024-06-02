39c86a9cfd9670d5cf8551a670328b4eea881f5a
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-02 14:07:43 -0400

    Update variables.tf

--- VPC servicio/terrform-code2/variables.tf
+++ VPC servicio/terrform-code2/variables.tf
@@ -1,1 +1,45 @@
+## VPC
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_tags" {
+  description = "Tags for the VPC"
+  type        = map(string)
+}
+
+## Subnet public
+
+variable "public_subnet_cidr_blocks" {
+  description = "CIDR blocks for the public subnets"
+  type        = list(string)
+}
+
+variable "public_azs" {
+  description = "Availability Zones for the public subnets"
+  type        = list(string)
+}
+
+variable "public_subnet_tags" {
+  description = "Tags for the public subnets"
+  type        = map(string)
+}
+
+## Subnet private
+
+variable "private_subnet_cidr_blocks" {
+  description = "CIDR blocks for the private subnets"
+  type        = list(string)
+}
+
+variable "private_azs" {
+  description = "Availability Zones for the private subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_tags" {
+  description = "Tags for the private subnets"
+  type        = map(string)
+}
 
