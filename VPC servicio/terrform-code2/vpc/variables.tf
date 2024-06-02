5055e63feec27d0009df769bbbd679e9652dd9c6
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-02 13:58:38 -0400

    Update variables.tf

--- VPC servicio/terrform-code2/vpc/variables.tf
+++ VPC servicio/terrform-code2/vpc/variables.tf
@@ -1,1 +1,14 @@
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_tags" {
+  description = "Tags for the VPC"
+  type        = map(string)
+  default     = {
+    Name        = "MyVPC"
+    Environment = "Production"
+  }
+}
 
