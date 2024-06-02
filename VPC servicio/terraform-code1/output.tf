0ab2e3563bff303b53620a2fa251bd29343bf69e
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-02 13:41:37 -0400

    Update output.tf

--- VPC servicio/terraform-code1/output.tf
+++ VPC servicio/terraform-code1/output.tf
@@ -1,1 +1,27 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public_subnets[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private_subnets[*].id
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.gw.id
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.public-rt.id
+}
+
+output "private_route_table_ids" {
+  value = [
+    aws_route_table.private-rt1.id,
+    aws_route_table.private-rt2.id
+  ]
+}
 
