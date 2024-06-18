35e8aa32d9974dad7c45ead51ca8fc693c090634
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-17 22:30:21 -0400

    Update main.tf

--- S3 Service/terrafomr-code/main.tf
+++ S3 Service/terrafomr-code/main.tf
@@ -1,1 +1,39 @@
+# Creacion S3 Bucket
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+  tags   = var.tags
+}
 
+# Configuración de ciclo de vida
+resource "aws_s3_bucket_lifecycle_configuration" "s3_bucket_lifecycle" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  rule {
+    id     = "log"
+    status = "Enabled"
+
+    filter {}
+
+    transition {
+      days          = 30
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 365
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+  }
+}
+
+# Activar versionado
+resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
