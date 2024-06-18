01f883e7d811905bfa82952b0852d7baeeb14ed2
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-17 22:31:24 -0400

    Update provider.tf

--- S3 Service/terrafomr-code/provider.tf
+++ S3 Service/terrafomr-code/provider.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 
+  required_version = ">= 1.2.0"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-1" 
+  access_key = "A" 
+  secret_key = "E"
+}
