e1effbf9b4391ff88fe2ac0a22a9fe48cf2dac15
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-17 22:30:44 -0400

    Update variables.tf

--- S3 Service/terrafomr-code/variables.tf
+++ S3 Service/terrafomr-code/variables.tf
@@ -1,1 +1,14 @@
+variable "bucket_name" {
+  description = "El nombre del bucket de S3"
+  type        = string
+  default     = "andherson-s3-demo-xyz"
+}
 
+variable "tags" {
+  description = "Etiquetas para el bucket de S3"
+  type        = map(string)
+  default     = {
+    Environment = "Dev"
+    Project     = "S3Demo"
+  }
+}
