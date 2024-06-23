fd5e486e220fac5791ed022c9f48da58527c245d
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-22 23:30:10 -0400

    Update backend.tf

--- S3 backend remote/terrafom-code/backend.tf
+++ S3 backend remote/terrafom-code/backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "andherson-s3-demo-xyz" # Cambia esto al nombre de tu bucket S3
+    key            = "andherson/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
