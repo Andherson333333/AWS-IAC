31d2da4b15026b24050ce2a686334c77db766f65
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-22 23:29:54 -0400

    Update main.tf

--- S3 backend remote/terrafom-code/main.tf
+++ S3 backend remote/terrafom-code/main.tf
@@ -1,1 +1,22 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "andherson-s3-demo-xyz"
+}
 
+resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
