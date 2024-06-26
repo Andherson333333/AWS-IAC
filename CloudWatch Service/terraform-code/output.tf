cd8d25fe12da466126a8d4f16252d5f77ebc61b4
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-26 09:15:04 -0400

    Update output.tf

--- CloudWatch Service/terraform-code/output.tf
+++ CloudWatch Service/terraform-code/output.tf
@@ -1,14 +1,14 @@
 output "alarm_arn" {
-  description = "The ARN of the CloudWatch alarm"
+  description = "ARN  CloudWatch alarm"
   value       = aws_cloudwatch_metric_alarm.alarm.arn
 }
 
 output "sns_topic_arn" {
-  description = "The ARN of the SNS topic"
+  description = "Arn Topic"
   value       = aws_sns_topic.sns_send.arn
 }
 
 output "dashboard_name" {
-  description = "The name of the CloudWatch dashboard"
+  description = "Nombre dashboard"
   value       = aws_cloudwatch_dashboard.example.dashboard_name
 }
