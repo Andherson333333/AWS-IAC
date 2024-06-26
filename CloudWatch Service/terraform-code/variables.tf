1476256031182ca64ffb8e20f408bc78beb9d0f6
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-26 09:02:13 -0400

    Update variables.tf

--- CloudWatch Service/terraform-code/variables.tf
+++ CloudWatch Service/terraform-code/variables.tf
@@ -1,1 +1,9 @@
+variable "instance_id" {
+  description = "The ID of the EC2 instance to monitor"
+  type        = string
+}
 
+variable "sns_email" {
+  description = "The email address to subscribe to the SNS topic"
+  type        = string
+}
