0e1224ed61160e683c7fcc6085bc15e01fb91cd0
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-26 22:01:51 -0400

    Update variables.tf

--- EC2 servicio/terraform-code/variables.tf
+++ EC2 servicio/terraform-code/variables.tf
@@ -1,26 +1,21 @@
 variable "ami_id" {
   description = "ID de la AMI para la instancia EC2"
-  default     = "ami-058bd2d568351da34"
 }
 
 variable "instance_type" {
   description = "Tipo de instancia EC2"
-  default     = "t2.micro"
 }
 
 variable "key_name" {
   description = "Nombre de la clave SSH"
-  default     = "legion33"
 }
 
 variable "security_groups" {
   description = "Lista de grupos de seguridad"
   type        = list(string)
-  default     = ["sg-00ef3880ebb0de4a3"]
 }
 
 variable "subnet_id" {
   description = "ID de la subred"
-  default     = "subnet-006c3fe4de694a734"
 }
 
