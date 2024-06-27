d81b90b85f27c693230299b39268270a240b05cf
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-26 22:05:49 -0400

    Update variables.tf

--- S3 backend remote/terrafom-code/variables.tf
+++ S3 backend remote/terrafom-code/variables.tf
@@ -1,23 +1,20 @@
 variable "bucket_name" {
   description = "El nombre del bucket de S3"
   type        = string
-  default     = "andherson-s3-demo-xyz"
+  
 }
 
 variable "dynamodb_table_name" {
   description = "El nombre de la tabla de DynamoDB para el bloqueo"
   type        = string
-  default     = "terraform-lock"
 }
 
 variable "region" {
   description = "La región de AWS"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "state_key" {
   description = "La clave del estado de Terraform en el bucket S3"
   type        = string
-  default     = "andherson/terraform.tfstate"
 }
