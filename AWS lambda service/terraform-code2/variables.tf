3e01e7c7f831d1185aa54414ab6cfbc51c401de1
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-07-11 11:41:48 -0400

    Update variables.tf

--- AWS lambda service/terraform-code2/variables.tf
+++ AWS lambda service/terraform-code2/variables.tf
@@ -1,1 +1,31 @@
+# variables.tf
 
+variable "aws_region" {
+  description = "The AWS region to deploy to"
+  type        = string
+}
+
+variable "lambda_role_name" {
+  description = "Name of the IAM role for Lambda"
+  type        = string
+}
+
+variable "lambda_filename" {
+  description = "The name of the Lambda deployment package"
+  type        = string
+}
+
+variable "lambda_function_name" {
+  description = "The name of the Lambda function"
+  type        = string
+}
+
+variable "lambda_handler" {
+  description = "The Lambda function handler"
+  type        = string
+}
+
+variable "lambda_runtime" {
+  description = "The Lambda function runtime"
+  type        = string
+}
