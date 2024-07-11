92cb0945f599e26aee4d4fa5bb1fda760517efc7
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-07-11 09:42:06 -0400

    Update variables.tf

--- AWS lambda service/terraform-code1/variables.tf
+++ AWS lambda service/terraform-code1/variables.tf
@@ -1,1 +1,37 @@
+# variables.tf
 
+variable "lambda_role_name" {
+  description = "Name of the IAM role for Lambda"
+  type        = string
+}
+
+variable "lambda_filename" {
+  description = "The name of the Lambda deployment package"
+  type        = string
+}
+
+variable "lambda_function_name" {
+  description = "The name of the Lambda function"
+  type        = string
+}
+
+variable "lambda_handler" {
+  description = "The Lambda function handler"
+  type        = string
+}
+
+variable "lambda_runtime" {
+  description = "The Lambda function runtime"
+  type        = string
+}
+
+variable "lambda_environment_variables" {
+  description = "Environment variables for the Lambda function"
+  type        = map(string)
+  default     = {}
+}
+
+variable "lambda_code" {
+  description = "The Lambda function code"
+  type        = string
+}
