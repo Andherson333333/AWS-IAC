715fcff5044ba6e571cb48ef050782110086da16
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-07-11 11:41:37 -0400

    Update main.tf

--- AWS lambda service/terraform-code2/main.tf
+++ AWS lambda service/terraform-code2/main.tf
@@ -1,1 +1,30 @@
+resource "aws_iam_role" "lambda_role" {
+  name = var.lambda_role_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  output_path = var.lambda_filename
+  source_file = "index.js"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = var.lambda_function_name
+  role             = aws_iam_role.lambda_role.arn
+  handler          = var.lambda_handler
+  runtime          = var.lambda_runtime
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
