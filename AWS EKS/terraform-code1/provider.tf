358b0ce5be5ebe9db2f35e64b73771e2ac8e55d0
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-09-03 18:29:05 +0200

    Update provider.tf

--- AWS EKS/terraform-code1/provider.tf
+++ AWS EKS/terraform-code1/provider.tf
@@ -1,1 +1,23 @@
+# Provider Config
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.9.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
 
+provider "aws" {
+  alias  = "virginia"
+  region = local.region
+}
