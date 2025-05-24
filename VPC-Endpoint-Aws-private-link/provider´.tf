973a8e2db86e274ee1882d46619c9929748057ac
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-05-24 13:58:29 -0400

    Create provider´.tf

---
+++ VPC-Endpoint-Aws-private-link/provider´.tf
@@ -0,0 +1,17 @@
+################################################################
+# Provider
+################################################################
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
+  alias  = "virginia"
+  region = local.region
+}
