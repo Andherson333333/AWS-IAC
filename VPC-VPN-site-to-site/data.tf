35d630088fe65853e78cb84cd9859a46885e5c1c
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2025-06-16 14:04:46 -0400

    Create data.tf

---
+++ VPC-VPN-site-to-site/data.tf
@@ -0,0 +1,22 @@
+################################################################
+# Data Sources
+################################################################
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_ami" "amazon_linux2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
